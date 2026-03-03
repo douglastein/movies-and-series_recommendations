d79014058c513ebfb919eec40b82287eb54810bd
Douglas Stein Sabino <dsteinsabino@gmail.com>
2026-03-02 23:51:48 -0300

    feat: usuário pode testar mais de um título por uso

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 from src.vectorizer import generate_similarity_matrix
 from src.recommender import recommendation_system
 
+def main():
+    print("Sistema de Recomendação de filmes e séries")
+    print("Digite 'sair' para encerrar.\n")
+
+    df, similarity = generate_similarity_matrix(
+        "data/processed/netflix_titles_processed.csv"
+    )
+
+    while True:
+        titulo = input("Digite o nome de um filme ou série: ")
+
+        if titulo.lower() == "sair":
+            print("\nEncerrando sistema.")
+            break
+
+        recommendations = recommendation_system(titulo, df, similarity)
+        print("\nRecomendações:")
+        for r in recommendations:
+            print(f"- {r}")
+        print()
+
+
 if __name__ == "__main__":
-    df, similaridades = generate_similarity_matrix(r"data/processed/netflix_titles_processed.csv")
-
-    # titulo = "The Walking Dead"
-    titulo = str(input("Digite o nome de um filme/série: ")).strip()
-
-    recomendacoes = recommendation_system(titulo, df, similaridades)
-
-    for recs in recomendacoes:
-        print(recs)
-    
+    main()
